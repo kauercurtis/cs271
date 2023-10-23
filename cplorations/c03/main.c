55dc72b880a075b3509d82194f5f608960c4004f
Curtis Kauer <kauercurtis@gmail.com>
2023-10-22 17:00:04 -0700

    cleaned up some debugging statements in cploration3

--- cplorations/c03/main.c
+++ cplorations/c03/main.c
@@ -13,12 +13,12 @@
 
 int main(int argc, const char *argv[])
 {	
-	//printf("program starts\n");
+
 	if(argc != 2){
 		printf("Usage: %s [filename]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	//printf("opening file\n");
+
 	FILE *fin = fopen(argv[1], "r");
 
 	if(fin == NULL){
@@ -26,25 +26,16 @@ int main(int argc, const char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	//printf("declaring output file\n");
-	char *out_file = malloc(sizeof(char) * 500000);
-
-	//printf("copying to output file\n");
+	char *out_file = malloc(sizeof(char) * 128);
 
 	strcpy(out_file, argv[1]);
-	//printf("%s", out_file);
-	//printf("stringcat\n");
 	strcat(out_file, ".echo\0");
-	
-	//printf("%s", out_file);
 
 	FILE *fout = fopen(out_file, "w+");
 
-	//printf("initializing line string\n");
 	char line[MAX_LINE_LENGTH] = {0};
 	unsigned int line_num = 0;
 
-	//printf("entering while loop\n");
 	while(fgets(line, sizeof(line), fin)){
 		line_num++;
 		printf("[%04d] ", line_num);
