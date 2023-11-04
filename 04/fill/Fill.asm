58f24886a3a3b9f78994b70d625a43970e9938d1
Curtis Kauer <kauercurtis@gmail.com>
2023-11-04 02:43:23 -0700

    nand2tetris project 4 Mult.asm and Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,12 +3,69 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
+// Name: Curtis Kauer
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
 // the screen should remain fully black as long as the key is pressed. 
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// the screen should remain fully clear as long as no key is pressed.  
 
-// Put your code here.
+// resets pixel variable to 16384
+(REPIXEL)
+    @16384
+    D=A
+    @pixel
+    M=D
+    
+    //if keyboard > 0, jump to BLACK
+    @KBD
+    D=M
+    @BLACK
+    D;JGT
+
+//WHITE - fills screen with white
+(WHITE)
+    @pixel
+    D=M
+    @SCREEN
+    A=D
+    M=0
+
+    //if pixel - 24575 == 0, reset pixel, jump to REPIXEL
+    @24575
+    D=A
+    @pixel
+    D=D-M
+    @REPIXEL
+    D;JEQ
+
+    //else increment pixel, jump to WHITE
+    @pixel
+    M=M+1
+    @WHITE
+    0;JMP
+
+// BLACK - fills screen with black
+(BLACK)
+    @pixel
+    D=M
+    @SCREEN
+    A=D
+    M=-1
+
+    //if pixel - 24575 == 0, reset pixel, jump to REPIXEL
+    @24575
+    D=A
+    @pixel
+    D=D-M
+    @REPIXEL
+    D;JEQ
+
+    //else increment pixel, jump to BLACK
+    @pixel
+    M=M+1
+    @BLACK
+    0;JMP
+
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -3,10 +3,64 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Mult.asm
 
+// Name: Curtis Kauer
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    //n = 0
+    @n
+    M=0
+
+    //counter=0
+    @counter
+    M=0
+
+    //set RAM2 to 0
+    @R2
+    M=0
+
+(LOOP)
+    //if R0 == 0 OR R1 == 0 Go to End
+    @n
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JEQ
+
+    @n
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JEQ
+
+    //if counter == R1 go to END
+    @counter
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JEQ
+
+    //else add value at RAM0 to RAM2
+    @R0
+    D=M
+    
+    @R2
+    M=D+M
+
+    //increment counter
+    @counter
+    D=M
+    M=D+1
+
+    //Loop
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
