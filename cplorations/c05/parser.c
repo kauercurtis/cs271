6f1c8f4501067dcc3399b7b67ab22b954163da8e
Curtis Kauer <kauercurtis@gmail.com>
2023-11-05 17:31:16 -0800

    cploration 5

---
+++ cplorations/c05/parser.c
@@ -0,0 +1,111 @@
+/****************************************
+ * C-ploration 5 for CS 271
+ * 
+ * [NAME] Curtis Kauer
+ * [TERM] FALL 2023
+ * 
+ ****************************************/
+#include "parser.h"
+
+/* Function: strip
+ * -------------
+ * remove whitespace and comments from a line
+ *
+ * s: the char* string to strip
+ *
+ * returns: the stripped char* string
+ */
+char *strip(char *s){	
+	char s_new[strlen(s) + 1];
+	unsigned indexCounter = 0;
+	for(char *s2 = s; *s2; s2++){
+		if(*(s2 + 0) == '/' && *(s2 + 1) == '/'){
+			break;
+		}
+		else if(!isspace(*(s2))){
+			s_new[indexCounter++] = *s2;
+		}
+	}
+
+	s_new[indexCounter] = '\0';
+
+	strcpy(s, s_new);
+
+    return s;	
+}
+
+/* Function: parse
+ * -------------
+ * iterate each line in the file and strip whitespace and comments. 
+ *
+ * file: pointer to FILE to parse
+ *
+ * returns: nothing
+ */
+void parse(FILE * file){
+	
+	char inst_type; 
+	
+	char line[MAX_LINE_LENGTH] = {0};
+
+	while(fgets(line, sizeof(line), file)){
+		strip(line);
+		if(*line == '\0'){
+			continue;
+		}
+		if(is_Atype(line)){
+			inst_type = 'A';
+		}
+		else if(is_label(line)){
+			inst_type = 'L';
+		}
+		else if(is_Ctype(line)){
+			inst_type = 'C';
+		}
+		printf("%c  %s\n", inst_type, line);
+	} 
+	
+}
+
+/*
+is_Atype - determines if a line is interacting with A register
+param1 - const char *line - the line to determine
+return - bool - if the line is A register type, return true, false if any other case
+*/
+bool is_Atype(const char *line){
+	if(*line == '@'){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+/*
+is_label - determines the given line is a identifier of a group
+param1 - const char *line - line to determine
+return - bool - if the line starts with ( and ends with ), return true, otherwise return false
+*/
+bool is_label(const char *line){
+	unsigned length = strlen(line);
+	if(*line == '(' && line[length - 1] == ')'){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+/*
+is_Ctype - determines if the the given line is a C instruction
+param1 - const char *line - line to determine
+return - bool - if the start of the line is NOT ( AND NOT @, return true, otherwise return false
+*/
+bool is_Ctype(const char *line){
+	if(*line != '(' && *line != '@'){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
